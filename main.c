31582a552630d6b8a6d2f884cea268e2d22b0f29
Abril Nataly Calatayud <acalatayud@fie.undef.edu.ar>
2023-06-23 12:10:21 -0300

    entrega final

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-
+#include "ejercicio-1-parcial.h"
 int main() {
-    printf("Hello, World!\n");
+    lista * lista1 = newLista();
+    lista * lista2 = newLista();
+
+    enlistar(lista1, newNodo(3));
+    enlistar(lista1, newNodo(5));
+    enlistar(lista1, newNodo(4));
+    enlistar(lista1, newNodo(7));
+    enlistar(lista1, newNodo(8));
+
+    enlistar(lista2, newNodo(5));
+    enlistar(lista2, newNodo(4));
+    enlistar(lista2, newNodo(8));
+    enlistar(lista2, newNodo(9));
+    enlistar(lista2, newNodo(10));
+
+    printf("Lista 1:\n");
+    imprimir(lista1);
+    printf("Lista 2:\n");
+    imprimir(lista2);
+
+    juntarEnOrden(lista1,lista2);
+    printf("Lista unida:\n");
+    imprimir(lista1);
     return 0;
 }
